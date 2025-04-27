33c5d5bf564f4f3bfb947802e6539f0ae2f6232f
Vedant <68198041+vedantA04@users.noreply.github.com>
2025-04-27 20:18:59 +0530

    Update config.js

--- src/app/resources/config.js
+++ src/app/resources/config.js
@@ -4,9 +4,9 @@ const baseURL = "https://demo.magic-portfolio.com";
 const routes = {
   "/": true,
   "/about": true,
-  "/work": true,
-  "/blog": true,
-  "/gallery": true,
+  "/work": false,
+  "/blog": false,
+  "/gallery": false,
 };
 
 // Enable password protection on selected routes
