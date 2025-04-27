bc93cccf0d4e3db28fcae0dd3a28d7ea0b3ed6a2
Vedant <68198041+vedantA04@users.noreply.github.com>
2025-04-27 20:59:43 +0530

    Update route.tsx

--- src/app/og/route.tsx
+++ src/app/og/route.tsx
@@ -1,1 +1,37 @@
+import { ImageResponse } from "next/og";
+import { baseURL } from "@/app/resources";
+import { person } from "@/app/resources/content";
 
+export const runtime = "edge";
+
+export async function GET(request: Request) {
+  let url = new URL(request.url);
+  let title = url.searchParams.get("title") || "Portfolio";
+  const font = fetch(new URL("../../../public/fonts/Inter.ttf", import.meta.url)).then((res) =>
+    res.arrayBuffer(),
+  );
+  const fontData = await font;
+
+  return new ImageResponse(
+    <div
+      style={{
+        display: "flex",
+        width: "100%",
+        height: "100%",
+        padding: "8rem",
+        background: "#151515",
+      }}
+    >,
+    {
+      width: 1920,
+      height: 1080,
+      fonts: [
+        {
+          name: "Inter",
+          data: fontData,
+          style: "normal",
+        },
+      ],
+    },
+  );
+}
